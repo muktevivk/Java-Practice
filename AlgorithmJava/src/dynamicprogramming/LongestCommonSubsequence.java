dd88c33de5bd17a984dd921f912e1567468d4a16
vm4q8 <vm4q8@KC-MNL2LAB-01.kc.umkc.edu>
2017-04-17 21:25:03 -0500

    LCS top down with memorization

--- AlgorithmJava/src/dynamicprogramming/LongestCommonSubsequence.java
+++ AlgorithmJava/src/dynamicprogramming/LongestCommonSubsequence.java
@@ -9,12 +9,32 @@ package dynamicprogramming;
  */
 public class LongestCommonSubsequence {
 
-	/**
-	 * @param args
-	 */
+	public static int[][] memo;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String s = "acbaed", t = "abcadf";
+		memo = new int[s.length()+1][t.length()+1];
+		int output = getLCSLen(s,t);
+		System.out.println("The lenght of LCS : "+output);
+	}
 
+	private static int getLCSLen(String s, String t) {
+		if (s== null || s.length() == 0 ){
+			return 1;
+		}
+		if (t== null || t.length() == 0 ){
+			return 1;
+		}
+		return getLCS(s.toCharArray(),s.length()-1,t.toCharArray(),t.length()-1);
+	}
+
+	private static int getLCS(char[] sArray, int n, char[] tArray, int m) {
+		if (n==0||m==0) return 1;
+		if (memo[n][m]!=0) return memo[n][m];
+		if (sArray[n] == tArray[m]) return memo[n][m] =  1+getLCS(sArray, n-1, tArray, m-1);
+		else return memo[n][m]= Math.max( getLCS(sArray, n-1, tArray, m),  getLCS(sArray, n, tArray, m-1));
+		
 	}
 
 }
