07bcf868412d9ff4e17342b3c2ae4446fa21358e
muktevi <vamsi.muktevi@hotmail.com>
2017-04-18 02:30:12 -0500

    Longest Common Substring

--- AlgorithmJava/src/dynamicprogramming/LongestCommonSubstring.java
+++ AlgorithmJava/src/dynamicprogramming/LongestCommonSubstring.java
@@ -3,6 +3,9 @@
  */
 package dynamicprogramming;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Muktevi
  *
@@ -14,26 +17,34 @@ public class LongestCommonSubstring {
 
 		String s = "LCLC", t = "CLCL";
 		memo = new int[s.length()+1][t.length()+1];
-		int output = getLCSubstring(s,t);
+		String output = getLCSubstring(s,t);
 		System.out.println("Longest common substring is: "+ output);
 	}
 
-	private static int getLCSubstring(String s, String t) {
+	private static String getLCSubstring(String s, String t) {
 
 		int lenS = s.length(), lenT = t.length(), maxLen = Integer.MIN_VALUE;
-		
+		List<String> result = null;
 		for (int i =0; i<lenS;i++){
 			for(int j = 0;j<lenT; j++){
 				if (s.charAt(i) == t.charAt(j)){
 					if (i==0 || j==0) memo[i][j] = 1;
 					else memo[i][j] = 1+memo[i-1][j-1];
+					if (memo[i][j]>maxLen){
+						maxLen = memo[i][j];
+						result = new ArrayList<String>();
+						result.add(s.substring(i-maxLen+1,i+1));
+					}
+					else if (memo[i][j]== maxLen){
+						result.add(s.substring(i-maxLen+1, i+1));
+					}
 				}
-				if (memo[i][j]>maxLen) maxLen = memo[i][j];
+				
 			}
 		}
 		
 		
-		return maxLen;
+		return result.toString();
 	}
 
 }
